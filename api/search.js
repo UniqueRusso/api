d929167fc46f6cb2ecb3c15a7f1f39d9f075fcca
UniqueRusso <130264502+UniqueRusso@users.noreply.github.com>
2025-05-29 04:06:03 +0200

    Update search.js

--- api/search.js
+++ api/search.js
@@ -1,4 +1,12 @@
 export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // Allow all origins
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   const { query } = req.query;
   const apiKey = process.env.SERP_API_KEY;
 
